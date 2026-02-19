6dbe19655e169f41f2257a662dbe46dcd299f9fc
kaveeshapasan2002 <it23292604@my.sliit.lk>
2026-02-19 14:19:34 +0530

    updated disposalactivity repository interface

--- src/domain/repositories/DisposalActivityRepository.js
+++ src/domain/repositories/DisposalActivityRepository.js
@@ -7,12 +7,28 @@ class DisposalActivityRepository {
     throw new Error('findById method must be implemented');
   }
 
-  async findByUserId(userId) {
+  async findByUserId(userId, populate = false) {
     throw new Error('findByUserId method must be implemented');
+  }
+
+  async findByWasteId(wasteId) {
+    throw new Error('findByWasteId method must be implemented');
+  }
+
+  async update(id, updates) {
+    throw new Error('update method must be implemented');
   }
 
   async deleteById(id) {
     throw new Error('deleteById method must be implemented');
+  }
+
+  async getAggregatedStats(startDate, endDate) {
+    throw new Error('getAggregatedStats method must be implemented');
+  }
+
+  async getTotalWasteByUser(userId, startDate, endDate) {
+    throw new Error('getTotalWasteByUser method must be implemented');
   }
 }
 
